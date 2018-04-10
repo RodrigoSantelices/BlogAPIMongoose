12fc192d607432541fa27c84126d8d8472dd33a0
Rodrigo Santelices <rcsantelices@gmail.com>
2018-04-09 17:34:50 -0700

    config.js changes

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://rod:Test1@ds135399.mlab.com:35399/blog-api';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://rod:Test1@ds135399.mlab.com:35399/blog-api/blogposts';
 exports.PORT = process.env.PORT || 8080;
